2177055a01ffaef655f857acdb2f53b671a35db6
xiaotiandada <istianlei@qq.com>
2021-06-04 11:14:20 +0800

    feat: 404 page

---
+++ pages/404.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    404 page ...
+    <n-link :to="{ name: 'index' }">
+      Go Home
+    </n-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class AuthGithubCallback extends Vue {
+  created () {
+    if (process.client) {
+      //
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.wrapper {
+  margin: 100px auto 0;
+  font-size: 20px;
+  text-align: center;
+}
+</style>
---
+++ pages/_.vue
@@ -0,0 +1,16 @@
+<template>
+  <div />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class AuthGithubCallback extends Vue {
+  created () {
+    if (process.client) {
+      this.$router.push('/404')
+    }
+  }
+}
+</script>
