16b723760ac2972dc485492402c8cb829bc95249
xiaotiandada <istianlei@qq.com>
2021-07-19 15:50:38 +0800

    Update env.js

--- env.js
+++ env.js
@@ -10,9 +10,9 @@ export default {
     APP_MATATAKI_TUTORIAL: 'https://www.matataki.io/p/9196',
     APP_SSIMG: 'https://ssimg.frontenduse.top',
     // # Matataki OAuth Related config
-    REACT_APP_OAuthUrl: 'https://developer.matataki.io/app/905ac873d9d71038/oauth?network=test',
-    REACT_APP_OAuthClientId: '401523152384315',
-    REACT_APP_OAuthClientSecret: '2987112115071a55',
+    REACT_APP_OAuthUrl: 'https://developer.matataki.io/app/018d841c87d74dbb/oauth?network=test',
+    REACT_APP_OAuthClientId: '788733784487663',
+    REACT_APP_OAuthClientSecret: 'd569b4a9f7392a95',
     VUE_APP_HCAPTCHA_SITE_KEY: 'add3227a-649b-4e14-b286-d6c917f9b775'
   },
   production: {
@@ -25,9 +25,9 @@ export default {
     APP_MATATAKI_TUTORIAL: 'https://www.matataki.io/p/9196',
     APP_SSIMG: 'https://ssimg.frontenduse.top',
     // # Matataki OAuth Related config
-    REACT_APP_OAuthUrl: 'https://developer.matataki.io/app/f1d77cd8b16bd25f/oauth',
-    REACT_APP_OAuthClientId: '582657204820772',
-    REACT_APP_OAuthClientSecret: '62694caddff0c216',
+    REACT_APP_OAuthUrl: 'https://developer.matataki.io/app/c94f997ea18a924a/oauth',
+    REACT_APP_OAuthClientId: '178876132608254',
+    REACT_APP_OAuthClientSecret: '6b076aba3b4cac84',
     VUE_APP_HCAPTCHA_SITE_KEY: '929510ae-ff3e-4ccc-bb8b-f7dd3983e791'
   }
 }
