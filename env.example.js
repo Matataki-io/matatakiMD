ada25cb9e660d554cc4be1c15d71b4f6b61d5fe0
xiaotiandada <istianlei@qq.com>
2022-03-22 16:17:36 +0800

    feat: env example

---
+++ env.example.js
@@ -0,0 +1,47 @@
+
+export default {
+  development: {
+    NODE: 'development',
+    NODE_ENV: 'development',
+    // Backend Url
+    APP_API_URL: '',
+    // Matataki Url
+    APP_MATATAKI_URL: '',
+    // Matataki MD Release Notes Url
+    APP_MATATAKI_RELEASE_NOTES: '',
+    // Matataki MD Tutorial Url
+    APP_MATATAKI_TUTORIAL: '',
+    // Matataki IMG OSS Bucket Url
+    APP_SSIMG: '',
+    // Matataki OAuth Url
+    REACT_APP_OAuthUrl: '',
+    // Matataki OAuth ID
+    REACT_APP_OAuthClientId: '',
+    // Matataki OAuth Secret
+    REACT_APP_OAuthClientSecret: '',
+    // HCAPTCHA SITE KEY
+    VUE_APP_HCAPTCHA_SITE_KEY: ''
+  },
+  production: {
+    NODE: 'production',
+    NODE_ENV: 'production',
+    // Backend Url
+    APP_API_URL: '',
+    // Matataki Url
+    APP_MATATAKI_URL: '',
+    // Matataki MD Release Notes Url
+    APP_MATATAKI_RELEASE_NOTES: '',
+    // Matataki MD Tutorial Url
+    APP_MATATAKI_TUTORIAL: '',
+    // Matataki IMG OSS Bucket Url
+    APP_SSIMG: '',
+    // Matataki OAuth Url
+    REACT_APP_OAuthUrl: '',
+    // Matataki OAuth ID
+    REACT_APP_OAuthClientId: '',
+    // Matataki OAuth Secret
+    REACT_APP_OAuthClientSecret: '',
+    // HCAPTCHA SITE KEY
+    VUE_APP_HCAPTCHA_SITE_KEY: ''
+  }
+}
